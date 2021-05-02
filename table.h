8cb87c8fc2469909dcab954d748fd5c0cbb9be6f
Taksh Patel <82723451+Taksh13@users.noreply.github.com>
2021-05-02 17:32:19 -0400

    Update table.h

--- table.h
+++ table.h
@@ -7,9 +7,9 @@
 
 #include "hash.h"
 
-
-typedef struct hash_table_item hash_table_item;
-struct hash_table_item {
+typedef struct tableItem tableItem;
+struct tableItem
+{
     char *key;
     char *value;
     uint8_t is_deleted;
@@ -17,20 +17,20 @@ struct hash_table_item {
 };
 
 typedef struct hash_table hash_table;
-struct hash_table {
+struct hash_table
+{
     uint64_t size;
     uint64_t used_items_count;
     uint64_t not_deleted_used_items_count;
     hash_function_params hash_params;
-    hash_table_item *items;
+    tableItem *items;
 };
 
-
-void hash_table_init(hash_table *table, uint64_t size);
-uint64_t hash_table_insert(hash_table *table, char *key, char *value);
-hash_table_item *hash_table_search(hash_table *table, char *key);
-uint8_t hash_table_delete_item_by_key(hash_table *table, char *key);
-void hash_table_destroy(hash_table *table, uint8_t clear_keys);
-uint8_t hash_table_extend(hash_table *table, uint64_t new_size);
+void startHashTable(hash_table *table, uint64_t size);
+uint64_t insertHashTable(hash_table *table, char *key, char *value);
+tableItem *searchHashTable(hash_table *table, char *key);
+uint8_t deleteItemKey(hash_table *table, char *key);
+void deleteHashTable(hash_table *table, uint8_t clear_keys);
+uint8_t extendHashTable(hash_table *table, uint64_t new_size);
 
 #endif
